8ec460f3cf635c8055438bb36212f61cd9ae08d9
Lu Sun <sunlufinal@hotmail.com>
2017-09-13 15:54:50 -0400

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Node.create(title: "Node 1 Title", text: "Node 1 Text");
+Node.create(title: "Node 2 Title", text: "Node 2 Text");
+Node.create(title: "Node 3 Title", text: "Node 3 Text");
+Node.create(title: "Node 4 Title", text: "Node 4 Text");
+Node.create(title: "Node 5 Title", text: "Node 5 Text");
+Node.create(title: "Node 6 Title", text: "Node 6 Text");
+Node.create(title: "Node 7 Title", text: "Node 7 Text");
+Node.create(title: "Node 8 Title", text: "Node 8 Text");
+Node.create(title: "Node 9 Title", text: "Node 9 Text");
+Node.create(title: "Node 10 Title", text: "Node 10 Text");
